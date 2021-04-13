50d63553ef592527268ff65733c7b8ccd2a35846
Andreia <andreia.machion@saojudas.br>
2021-04-13 18:35:24 -0300

    até listagem 2.4.6.4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component } from  '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'diretivas';
+  textoBotao =  "Esconder";
+  esconder = false;
+
+  pessoas = [
+    { nome: "José", idade: 18 },
+    { nome: "Maria", idade: 22 }
+  ];
+
+  alterarExibicao() {
+    this.textoBotao = this.esconder ? "Exibir": "Esconder";
+    this.esconder = !this.esconder;
+  }
+
+  adicionar(nome, idade) {
+    this.pessoas = [{ nome: nome, idade: idade }, ...this.pessoas];
+  }
 }
